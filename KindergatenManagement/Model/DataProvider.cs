13bd3ec5f4ff231370af3fd3d2d70c73687f53a7
T <trinhthinh388@gmail.com>
2019-03-27 21:09:33 +0700

    Add project files.

---
+++ KindergatenManagement/Model/DataProvider.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KindergatenManagement.Model
+{
+    class DataProvider
+    {
+        private static DataProvider _ins;
+        public static DataProvider Ins
+        {
+            get
+            {
+                if (_ins == null)
+                {
+                    return _ins = new DataProvider();
+                }
+                return _ins;
+            }
+
+            set
+            {
+                _ins = value;
+            }
+        }
+
+        public KindergartenManagementEntities db { get; set; }
+
+        private DataProvider()
+        {
+            db = new KindergartenManagementEntities();
+        }
+    }
+}
---
+++ KindergatenManagement/ViewModel/ControlBarViewModel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace KindergatenManagement.ViewModel
+{
+    public class ControlBarViewModel: BaseViewModel
+    {
+        #region commands
+        public ICommand CloseWindowCommand { get; set; }
+        public ICommand MinimizeWindowCommand { get; set; }
+        public ICommand DragMoveWindowCommand { get; set; }
+        #endregion
+
+        public ControlBarViewModel()
+        {
+            CloseWindowCommand = new RelayCommand<UserControl>((p)=> { return p == null ? false : true; }, (p)=> 
+            {
+               var window =  GetWindowParent(p) as Window;
+                if(window != null)
+                {
+                    window.Close();
+                }
+            });
+            MinimizeWindowCommand = new RelayCommand<UserControl>((p)=> { return p == null ? false : true; }, (p)=>
+            {
+                var window = GetWindowParent(p) as Window;
+                if (window != null)
+                {
+                    if (window.WindowState == WindowState.Normal)
+                        window.WindowState = WindowState.Minimized;
+                    else
+                        window.WindowState = WindowState.Normal;
+                }
+            });
+            DragMoveWindowCommand = new RelayCommand<UserControl>((p) => { return p == null ? false : true; }, (p) =>
+            {
+                var window = GetWindowParent(p) as Window;
+                if (window != null)
+                {
+                    window.DragMove();
+                }
+            });
+        }
+
+        private FrameworkElement GetWindowParent(UserControl p)
+        {
+            FrameworkElement parent = p;
+            while(parent.Parent != null)
+            {
+                parent = parent.Parent as FrameworkElement;
+            }
+            return parent;
+        }
+    }
+}
